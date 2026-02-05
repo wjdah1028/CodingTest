cbf0fbac7a8800909a1d383b3fc9a8e9c4784139
wjdah1028 <wjdah1028@naver.com>
2026-02-05 13:10:25 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String s = String.valueOf(n);
+        char[] c = s.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            answer += (c[i] - '0');
+        }
+        return answer;
+    }
+}
