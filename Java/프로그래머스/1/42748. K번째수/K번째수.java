d8a5648a813e0b755761eaf0a1f7b3326b10a642
wjdah1028 <wjdah1028@naver.com>
2026-02-25 11:09:39 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 99.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int n = 0; n < commands.length; n++) {
+            int i = commands[n][0];
+            int j = commands[n][1];
+            int k = commands[n][2];
+            int[] temp = Arrays.copyOfRange(array, i - 1, j);
+            Arrays.sort(temp);
+            answer[n] = temp[k - 1];
+        }
+        return answer;
+    }
+}
