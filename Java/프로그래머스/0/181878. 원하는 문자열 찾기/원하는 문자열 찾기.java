933572ae3c35a5f23fb0ffccdf05fbd9f6f1e7cf
wjdah1028 <wjdah1028@naver.com>
2026-03-08 11:05:40 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.64 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        myString = myString.toUpperCase();
+        pat = pat.toUpperCase();
+        answer = (myString.contains(pat) ? 1 : 0);
+        return answer;
+    }
+}
