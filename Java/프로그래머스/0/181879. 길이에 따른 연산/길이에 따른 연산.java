0af46191c19e0b00b96ece09c6e1d5ce05550a4b
wjdah1028 <wjdah1028@naver.com>
2026-03-09 09:52:14 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = (num_list.length <= 10) ? 1 : 0;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list.length <= 10) {
+                answer *= num_list[i];
+            }
+            else if (num_list.length >= 11) {
+                answer += num_list[i];
+            }
+        }
+        return answer;
+    }
+}
