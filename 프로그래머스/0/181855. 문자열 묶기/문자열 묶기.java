09b59ba270321e8f12426163c74cf134dfcbe98f
wjdah1028 <wjdah1028@naver.com>
2026-03-20 15:42:55 +0900

    [level 0] Title: 문자열 묶기, Time: 9.61 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int[] count = new int[31];
+        for (int i = 0; i < strArr.length; i++) {
+            int len = strArr[i].length();
+            count[len]++;
+        }
+        for (int i = 0; i < count.length; i++) {
+            answer = Math.max(answer, count[i]);
+        }
+        return answer;
+    }
+}
