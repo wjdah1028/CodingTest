d68ae329f5c4dea7bd96dabc63e80602e02c12f2
wjdah1028 <wjdah1028@naver.com>
2026-03-26 15:06:54 +0900

    [level 0] Title: 가까운 수, Time: 0.07 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (Math.abs(array[i] - n) < Math.abs(answer - n)) {
+                answer = array[i];
+            }
+            else if (Math.abs(array[i] - n) == Math.abs(answer - n)) {
+                if (array[i] < answer) {
+                    answer = array[i];
+                }
+            }
+        }
+        return answer;
+    }
+}
