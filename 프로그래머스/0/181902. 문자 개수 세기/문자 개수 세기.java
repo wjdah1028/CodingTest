6cf2b7d04a32dd953c879778641c39dbd4ab1d7d
wjdah1028 <wjdah1028@naver.com>
2026-04-06 09:51:59 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.13 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        char[] c = my_string.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] >= 'A' && c[i] <= 'Z') {
+                answer[c[i] - 'A']++; 
+            }
+            else if (c[i] >= 'a' && c[i] <= 'z') {
+                answer[c[i] - 'a' + 26]++;
+            }
+        }
+        return answer;
+    }
+}
