1ad566f87324bd4c8510520431acc5aa0617f904
wjdah1028 <wjdah1028@naver.com>
2026-04-09 09:44:59 +0900

    [level 0] Title: 커피 심부름, Time: 0.34 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for (int i = 0; i < order.length; i++) {
+            if (order[i].equals("iceamericano") || order[i].equals("americanoice")) {
+                answer += 4500;
+            }
+            else if (order[i].equals("hotamericano") || order[i].equals("americanohot")) {
+                answer += 4500;
+            }
+            else if (order[i].equals("icecafelatte") || order[i].equals("cafelatteice")) {
+                answer += 5000;
+            }
+            else if (order[i].equals("hotcafelatte") || order[i].equals("cafelattehot")) {
+                answer += 5000;
+            }
+            else if (order[i].equals("americano")) {
+                answer += 4500;
+            }
+            else if (order[i].equals("cafelatte")) {
+                answer += 5000;
+            }
+            else {
+                answer += 4500;
+            }
+        }
+        return answer;
+    }
+}
